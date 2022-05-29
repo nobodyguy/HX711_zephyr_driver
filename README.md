9fb50091a9267c539418e2b92f652104c26e6388
nobodyguy <4577368+nobodyguy@users.noreply.github.com>
2022-05-30 00:50:09 +0200

    Added HX711 driver for Zephyr v2.7

---
+++ README.md
@@ -0,0 +1,2 @@
+# Out of tree HX711 driver module for Zephyr RTOS
+*HX711 is a precision 24-bit analog-to-digital converter (ADC) designed for weigh scale applications.*
---
+++ drivers/sensor/hx711/hx711.h
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2020 George Gkinis
+ * Copyright (c) 2021 Jan Gnip
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_HX711_H_
+#define ZEPHYR_DRIVERS_SENSOR_HX711_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <device.h>
+#include <kernel.h>
+#include <drivers/sensor.h>
+#include <drivers/gpio.h>
+
+/* Additional custom attributes */
+enum hx711_attribute
+{
+    HX711_SENSOR_ATTR_SLOPE = SENSOR_ATTR_PRIV_START,
+	HX711_SENSOR_ATTR_GAIN = SENSOR_ATTR_PRIV_START + 1,
+	HX711_SENSOR_ATTR_SAMPLING_FREQUENCY = SENSOR_ATTR_PRIV_START + 2,
+ 	HX711_SENSOR_ATTR_OFFSET = SENSOR_ATTR_PRIV_START + 3,
+};
+
+enum hx711_channel
+{
+    HX711_SENSOR_CHAN_WEIGHT = SENSOR_CHAN_PRIV_START,
+};
+
+enum hx711_gain {
+	HX711_GAIN_128X = 1,
+	HX711_GAIN_32X,
+	HX711_GAIN_64X,
+};
+
+enum hx711_rate {
+	HX711_RATE_10HZ,
+	HX711_RATE_80HZ,
+};
+
+enum hx711_power {
+	HX711_POWER_ON,
+	HX711_POWER_OFF,
+};
+
+struct hx711_data {
+	const struct device *dev;
+	const struct device *dout_gpio;
+	const struct device *sck_gpio;
+	const struct device *rate_gpio;
+	struct gpio_callback dout_gpio_cb;
+	struct k_sem dout_sem;
+
+	int32_t reading;
+
+	int offset;
+	struct sensor_value slope;
+	char gain;
+	enum hx711_rate rate;
+	enum hx711_power power;
+};
+
+struct hx711_config {
+	gpio_pin_t dout_pin;
+	const char *dout_ctrl;
+	gpio_dt_flags_t dout_flags;
+
+	gpio_pin_t sck_pin;
+	const char *sck_ctrl;
+	gpio_dt_flags_t sck_flags;
+
+	gpio_pin_t rate_pin;
+	const char *rate_ctrl;
+	gpio_dt_flags_t rate_flags;
+};
+
+/**
+ * @brief Zero the HX711.
+ *
+ * @param dev Pointer to the hx711 device structure
+ * @param readings Number of readings to get average offset.
+ *        5~10 readings should be enough, although more are allowed.
+ * @retval The offset value
+ *
+ */
+int avia_hx711_tare(const struct device *dev, uint8_t readings);
+
+/**
+ * @brief Callibrate the HX711.
+ *
+ * Given a target value of a known weight the slope gets calculated.
+ * This is actually unit agnostic.
+ * If the target weight is given in grams, lb, Kg or any other weight unit,
+ * the slope will be calculated accordingly.
+ *
+ * @param dev Pointer to the hx711 device structure
+ * @param target Target weight in grams.
+ *        If target is represented in another unit (lb, oz, Kg) then the
+ *        value returned by sensor_channel_get() will represent that unit.
+ * @param readings Number of readings to take for calibration.
+ *        5~10 readings should be enough, although more are allowed.
+ * @retval The slope value
+ *
+ */
+struct sensor_value avia_hx711_calibrate(const struct device *dev,
+					 uint32_t target,
+					 uint8_t readings);
+
+/**
+ * @brief Set the HX711 power.
+ *
+ * @param dev Pointer to the hx711 device structure
+ * @param power one of HX711_POWER_OFF, HX711_POWER_ON
+ * @retval The current power state or ENOTSUP if an invalid value pow is given
+ *
+ */
+int avia_hx711_power(const struct device *dev, enum hx711_power power);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
